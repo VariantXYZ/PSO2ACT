ce5502924f536b6667e90b0f4c5510b761e09647
Alam Arias <Alam.GBC@gmail.com>
2016-04-29 18:23:07 -0400

    buildbot: add check for comlumn size

--- check_skills.py
+++ check_skills.py
@@ -9,14 +9,29 @@ rowi = 0
 f = open('PSO2ACT/skills.csv','r')
 reader = csv.reader(f, delimiter=',')
 for row in reader:
+    name = row[0]
+    ID = None
+    Type = None
+    Detail = None
+    if len(row) > 1:
+        ID = row[1]
+    if len(row) > 2:
+        Type = row[2]
+    if len(row) > 3:
+        Detail = row[3]
     rowi = rowi + 1
-    if row[1] in skills_id:
-        print("Issue at row %d, Dup ID: %s" % (rowi, row) )
+    if len(row) != 4:
+        print("row %d, wrong column size: %s" % (rowi, row) )
+    elif rowi != 1 and ID == None:
+        print("row %d, Invaild ID: %s" % (rowi, row) )
+    if ID in skills_id:
+        print("row %d, Dup ID: %s" % (rowi, row) )
         status = status + 1
-    if rowi != 1 and not row[1].isdigit():
-        print("Issue at row %d, No ID: %s" % (rowi, row) )
+    if rowi != 1 and ID and  not ID.isdigit():
+        print("row %d, No ID: %s" % (rowi, row) )
         status = status + 1
-    skills_id.add(row[1])
+    if ID != None:
+        skills_id.add(ID)
 
 if status != 0:
    sys.exit("Found Errors in skills.csv")
